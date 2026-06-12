5b4f737e202c7ad95264208d254014ddb17e7024
Evandro Pires Alves <evandro@ipb.pt>
2026-06-12 17:41:26 +0100

    Loading em progresso

--- acarta/acarta.js
+++ acarta/acarta.js
@@ -1,32 +1,86 @@
+// objetos da página
 const mycanvas = document.getElementById("mycanvas")
 const ctx = mycanvas.getContext("2d")
+const progressMeter = document.getElementById("loaderProgress")
 
-const scenes = [
+// configurações do jogo
+const fundosCenarios = [
     "acarta/imagens/casa2.png",
     "acarta/imagens/casa.png",
     "acarta/imagens/casa2.png",
     "acarta/imagens/casa2.png",
 ]
-
-function loadScene(id) {
-    const cenario = new Image()
-    cenario.src = scenes[id]
-    cenario.onload = () => {
-        ctx.drawImage(cenario, 0, 0)
-    }
+const cenarios = [
+]
+const floor = 423
+const mario = {
+    image: null
 }
 
-let sceneId = 0
-loadScene(sceneId)
+// inicializar o jogo
+loadGame().then( () => {
+    console.log(cenarios)
+    let sceneId = 0
+    loadScene(sceneId)
+})
+initMainActor()
 
+// eventos
 addEventListener("keyup", (ev) => {
-    if (sceneId < 3) sceneId = sceneId + 1
+    console.log(ev.code)
+    
+    if (ev.code === "ArrowRight") {
+        if (sceneId < 3) sceneId = sceneId + 1
+    }
+    else if (ev.code === "ArrowLeft") {
+        if (sceneId > 0) sceneId = sceneId -1
+    }
+
     loadScene(sceneId)
 })
 
+mycanvas.addEventListener("click", (ev) => {
+    const bound = mycanvas.getBoundingClientRect() 
+    const x = ev.clientX - bound.left
+    const y = ev.clientY - bound.top
+    console.log(x + " x " + y)
+    ctx.drawImage(mario.image, x, y)
+})
+/////////////////////////////////////////////////
 
+async function loadGame() {
+    // inicializa cenários
+    fundosCenarios.forEach(url => {
+        const cenario = {
+            image: new Image()
+        }
+        cenario.image.src = url
+        cenario.image.onload = () => {
+            cenarios.push(cenario)
+            progressMeter.value += 0.1
+        }
+    }); 
 
+    // inicializa atores
+    mario.image = new Image()
+    mario.image.src = "/acarta/imagens/supermario.png"
+    mario.image.onload = () => {
+        progressMeter.value = 1
+    }
+}
 
+/**
+ * inicializa o cenário pretendido
+ * @param {int} id 
+ */
+function loadScene(id) {
+    console.log(cenarios[id].image)
+    ctx.drawImage(cenarios[id].image, 0, 0)
+}
+
+function initMainActor() {
+    
+}
 
 
 function hitbox() {
